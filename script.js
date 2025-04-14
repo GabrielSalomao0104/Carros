867b532e1ec1d2ba63d605cb0a27037744eebc79
GabrielSalomao0104 <gsalomaob27@gmail.com>
2025-04-14 08:31:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var carros = ["Porsche", "Renault", "Fiat", "Chevrolet", "Hyundai", "Honda", "Mitsubishi", "Ferrari", "Ford", "Toyota", "BMW", "Lamborghini", "Volvo", "BYD", "Jaguar"]
+var carroEscolhido = parseInt(prompt("Digite um carro de 0 a 15"))
+document.write("Carro escolhido: " + carros[carroEscolhido])
